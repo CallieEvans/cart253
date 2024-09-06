5edb267760e309ad055dd4df1fff5358a092ade4
CallieEvans <datperson23@gmail.com>
2024-09-06 12:36:36 -0400

    Started the drawing learning material and created folder

---
+++ week2/drawing/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Drawing
+ * Callie Evans
+ * 
+ * Practicing the drawing functions
+ * 
+ */
+
+"use strict";
+
+/**
+ * Happens at the beginning of the program
+*/
+function setup() {
+
+
+}
+
+
+/**
+ * Loops at every frame that our program runs
+*/
+function draw() {
+  
+
+}
