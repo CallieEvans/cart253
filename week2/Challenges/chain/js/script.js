9646d2219f08532864b8568dccc102841dfa21ce
CallieEvans <datperson23@gmail.com>
2024-09-06 13:59:56 -0400

    Finished chain challenge

--- week2/Challenges/chain/js/script.js
+++ week2/Challenges/chain/js/script.js
@@ -13,7 +13,7 @@
 */
 function setup() {
     //Creating a canvas for our chain
-    createCanvas(640,640);
+    createCanvas(640, 640);
 
 }
 
@@ -23,22 +23,20 @@ function setup() {
 */
 function draw() {
     //add a blue background 
-   background(255, 250, 20);
+   background(255,250,20);
 
-//    drawGround();
-
-//    drawPyramid();
-    
-
+   drawChains();
 }
 
-/**
- * Draw the pyramid
-*/
-
-function drawChain(){
-  
-
-
+function drawChains(){
+    push();
+    noFill();
+    stroke('black');
+    strokeWeight(35);
+    ellipse(320,0, 175, 250);
+    ellipse(320,155, 175, 250);
+    ellipse(320,320, 175, 250);
+    ellipse(320,480, 175, 250);
+     ellipse(320,640, 175, 250);
+    pop();
 }
-
