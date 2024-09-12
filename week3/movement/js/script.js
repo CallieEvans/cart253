103e6cf6329cdd797fce274e5064905de2a01a04
CallieEvans <datperson23@gmail.com>
2024-09-12 15:45:44 -0400

    Creating the movement assignment

---
+++ week3/movement/js/script.js
@@ -0,0 +1,35 @@
+/**
+ * Movement
+ * Callie Evans
+ * 
+ * Learning about movement
+ * 
+ */
+
+"use strict";
+
+/**
+ * Create canva for our project
+*/
+function setup() {
+    //p5 function
+    
+createCanvas(640, 640);
+
+}
+
+
+/**
+ * Loops every frame
+*/
+function draw() {
+background(0);
+
+    //draw circle
+push();
+
+pop();
+  
+
+
+}
