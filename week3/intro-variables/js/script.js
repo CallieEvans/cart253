15e4eee2d08374f30cc2bd989c36239fd618bb5e
CallieEvans <datperson23@gmail.com>
2024-09-12 15:11:01 -0400

    Finish var intros

--- week3/intro-variables/js/script.js
+++ week3/intro-variables/js/script.js
@@ -9,12 +9,12 @@
 "use strict";
 
 /**
- * We created a canva for our project
+ * Create canva for our project
 */
 function setup() {
     //p5 function
     
-createCanvas(640, 480);
+createCanvas(640, 640);
 
 }
 
@@ -23,6 +23,16 @@ createCanvas(640, 480);
  * Loops every frame
 */
 function draw() {
+background(0);
+
+    //draw circle
+push();
+noStroke();
+fill('red');
+//centers with p5 variables (based on canva dimensions)
+ellipse(width / 2, height / 2, 200,200);
+ellipse(mouseX, mouseY , 200,200);
+pop();
   
 
 
