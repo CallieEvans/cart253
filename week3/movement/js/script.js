df8fd0ee1f627afa00d11af8914fd88be2738f13
CallieEvans <datperson23@gmail.com>
2024-09-12 15:58:08 -0400

    Added a model of velocity and acceleration with a fake bird

--- week3/movement/js/script.js
+++ week3/movement/js/script.js
@@ -8,27 +8,60 @@
 
 "use strict";
 
+let bird = {
+    x:120,
+    y:480,
+    size:50,
+    velocity:{
+        x: 0,
+        y: 0
+    },
+    minVelocity:{
+        x:-3,
+        y:-2
+
+    },
+    maxVelocity:{
+        x: 3,
+        y: 2
+    },
+    acceleration:{
+        x: 0.025,
+        y: -0.2
+    }
+};
+
 /**
  * Create canva for our project
 */
 function setup() {
-    //p5 function
     
-createCanvas(640, 640);
+createCanvas(480, 480);
 
 }
 
 
 /**
- * Loops every frame
+ * Move the bird and display him
 */
 function draw() {
-background(0);
+    background(0);
+    //move the bird
+    bird.velocity.x += bird.acceleration.x;
+    bird.velocity.y += bird.acceleration.y;
+    bird.velocity.x = constrain(bird.velocity.x, bird.minVelocity.x, bird.maxVelocity.x);
+    bird.velocity.y = constrain(bird.velocity.y, bird.minVelocity.y, bird.maxVelocity.y);
 
-    //draw circle
-push();
 
-pop();
+    bird.x = bird.x + bird.velocity.x;
+    bird.y = bird.y + bird.velocity.y;
+
+    //create the bird
+    push();
+    fill(255,255,0);
+    noStroke();
+    ellipse(bird.x,bird.y,bird.size);
+    pop();
   
 
 
