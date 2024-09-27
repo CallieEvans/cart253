118c77cf779c8984546053dcc3e9dab477b321ef
CallieEvans <datperson23@gmail.com>
2024-09-27 11:20:13 -0400

    FInished intro to events

--- week5/events-tut/js/script.js
+++ week5/events-tut/js/script.js
@@ -11,13 +11,22 @@
  * create canvas
 */
 function setup() {
-
+    createCanvas(400, 400);
+    background(0);
 }
 
 
 /**
- * draws
+ * nothing
 */
 function draw() {
 
+}
+
+function mousePressed() {
+    push();
+    fill('#fff');
+    noStroke();
+    ellipse(mouseX, mouseY, 50);
+    pop();
 }
