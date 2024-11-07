3822994f35cdb2b6704ee7cf3986b9a89c9e985a
CallieEvans <datperson23@gmail.com>
2024-11-07 17:09:40 -0500

    Finished tarot project json

--- week10/tarot/js/script.js
+++ week10/tarot/js/script.js
@@ -6,18 +6,44 @@
  */
 
 "use strict";
+//tarot data
+let tarot = undefined;
+
+let fortune = 'click to show your fortune';
 
 /**
- * tbd.
+ * load tarot data
+ */
+function preload() {
+    tarot = loadJSON('data/tarot.json');
+}
+
+/**
+ * Create canvas
 */
 function setup() {
-
+    createCanvas(800, 400);
 }
 
 
 /**
- * tbd.
+ * Display tarot
 */
 function draw() {
+    background(0);
 
+    //display json
+    push();
+    textSize(16);
+    fill('yellow');
+    textAlign(CENTER, CENTER);
+    text(fortune, width / 2, height / 2);
+    pop();
+
+}
+
+function mousePressed() {
+    const card = random(tarot.tarot_interpretations);
+
+    fortune = random(card.fortune_telling);
 }
